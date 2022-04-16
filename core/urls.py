5bf1ff851f8b546edf376d49ff8511a237b92bcf
Md. Mosarrof Hossen <66587926+MosarrofSakib@users.noreply.github.com>
2022-04-16 20:52:55 +0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # re_path('.*', TemplateView.as_view(template_name='index.html')),
@@ -12,7 +12,8 @@ urlpatterns = [
     path('auth/', include('user.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-] 
- 
+]
+
 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
